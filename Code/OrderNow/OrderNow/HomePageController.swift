a802fa6e281617aa7cfda233fb12d08aff8c4ee9
ducle6487 <ducle6487@gmail.com>
2019-08-24 10:12:40 +0700

    no message

---
+++ Code/OrderNow/OrderNow/HomePageController.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageController.swift
+//  OrderNow
+//
+//  Created by Duc'sMacBook on 8/24/19.
+//  Copyright © 2019 Nguyen Thanh Phong. All rights reserved.
+//
+
+import UIKit
+
+class HomePageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
