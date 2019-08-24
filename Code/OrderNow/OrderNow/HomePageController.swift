b87626231b5f47f58ae628a005654bd80b7d04eb
ducle6487 <ducle6487@gmail.com>
2019-08-24 14:43:14 +0700

    update navigationbar

--- Code/OrderNow/OrderNow/HomePageController.swift
+++ Code/OrderNow/OrderNow/HomePageController.swift
@@ -9,22 +9,64 @@
 import UIKit
 
 class HomePageController: UIViewController {
-
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    var searchBar = UISearchBar()
+    @IBOutlet weak var searchBt: UIButton!
+    @IBOutlet weak var backBt: UIButton!
+    var cancelBt = UIButton()
+    @IBOutlet weak var UserImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        cancelBt.translatesAutoresizingMaskIntoConstraints = false
+        navigationBar.addSubview(searchBar)
+        navigationBar.addSubview(searchBt)
+        navigationBar.addSubview(cancelBt)
+        setUpContraint()
+        searchBar.isHidden = true
+        cancelBt.isHidden = true
+        cancelBt.addTarget(self, action: #selector(handleCancel)
+            , for: .touchUpInside)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setUpContraint(){
+        NSLayoutConstraint(item: searchBar, attribute: .leading, relatedBy: .equal, toItem: navigationBar, attribute: .leading, multiplier: 1.0, constant: 10).isActive = true
+        NSLayoutConstraint(item: searchBar, attribute: .centerY, relatedBy: .equal, toItem: navigationBar, attribute: .centerY, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: searchBar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 30).isActive = true
+        NSLayoutConstraint(item: cancelBt, attribute: .centerY, relatedBy: .equal, toItem: navigationBar, attribute: .centerY, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: cancelBt, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: -10).isActive = true
+        NSLayoutConstraint(item: cancelBt, attribute: .leading, relatedBy: .equal, toItem: searchBar, attribute: .trailing, multiplier: 1.0, constant: 5).isActive = true
+        searchBar.placeholder = "Tìm Kiếm"
+        cancelBt.setTitle("Cancel", for: .normal)
+        cancelBt.contentVerticalAlignment = .center
+        cancelBt.setTitleColor(UIColor.black, for: .normal)
     }
-    */
-
+    
+    @IBAction func searchAct(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.setUpTF(Bool: false)
+        }
+    }
+    
+    func setUpTF(Bool: Bool){
+        self.backBt.isHidden = !Bool
+        self.searchBt.isHidden = !Bool
+        self.searchBar.isHidden = Bool
+        self.cancelBt.isHidden = Bool
+    }
+    
+    @objc func handleCancel(){
+        DispatchQueue.main.async {
+            self.setUpTF(Bool: true)
+        }
+    }
+    @IBAction func nameUserAct(_ sender: Any) {
+    }
+    @IBAction func tapToImgUserAct(_ sender: Any) {
+    }
+    
 }
+
--- Code/OrderNow/OrderNow/Other/AppDelegate.swift
+++ Code/OrderNow/OrderNow/Other/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootView: HomePageController = HomePageController()
+        if let window = self.window{
+            window.rootViewController = rootView
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
