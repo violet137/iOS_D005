18cb4ec88b52fd18c88df44867f3db351b904cfb
Nghi Mac <nghi.duong@bizcellsoft.com>
2019-08-28 20:15:02 +0700

    login

---
+++ Code/OrderNow/OrderNow/Controller/LoginController/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  OrderNow
+//
+//  Created by ADMIN on 8/26/19.
+//  Copyright © 2019 Nguyen Thanh Phong. All rights reserved.
+//
+
+import UIKit
+import FacebookCore
+import FacebookLogin
+import FBSDKCoreKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var phoneView: UITextField!
+    @IBOutlet weak var otpView: UITextField!
+    @IBOutlet weak var btnContinueView: UIButton!
+    @IBOutlet weak var btnResendOTPView: UIButton!
+    
+    var isResendOTP = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func evPhoneChange(_ sender: UITextField) {
+        if isResendOTP
+        {
+            isResendOTP = false
+            btnContinueView.setTitle("Gửi OTP", for: .init())
+        }
+    }
+    
+    @IBAction func actContinue(_ sender: Any) {
+        if !isResendOTP
+        {
+            isResendOTP = true
+            btnContinueView.setTitle("Đăng nhập", for: .init())
+        }
+    }
+    
+    @IBAction func actResendOTP(_ sender: Any) {
+    }
+    
+    @IBAction func actLofinFB(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn(permissions: [.adsRead,], viewController: self) { (LoginResult) in
+            switch(LoginResult){
+            case .cancelled:
+                
+                break;
+            case .failed(let error):
+                
+                break;
+            case .success(_,_,let token):
+                
+                break;
+                
+            }
+        }
+    }
+    
+}
--- Code/OrderNow/OrderNow/Other/AppDelegate.swift
+++ Code/OrderNow/OrderNow/Other/AppDelegate.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import FacebookCore
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FBSDKCoreKit.ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return FBSDKCoreKit.ApplicationDelegate.shared.application(app, open: url, options: options)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
