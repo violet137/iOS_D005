7a4bf9b4be36f5b2cb5e0b4361c73be3ad30c607
ducle6487 <ducle6487@gmail.com>
2019-09-06 18:57:17 +0700

    new

--- Code/OrderNow/OrderNow/HomePageController.swift
+++ Code/OrderNow/OrderNow/HomePageController.swift
@@ -425,9 +425,10 @@ class HomePageController: UIViewController, UICollectionViewDataSource, UICollec
                 listFoodCollectionView.reloadData()
         }else{
             popUpIsClose(status: false)
+            amount = 1
             amountLb.text = String(amount)
             popupFoodNameLb.text = "Tôm hùm nướng, rưới sốt bơ chanh tỏi"
-            costLb.text = "85000 đ"
+            costLb.text = "\(amount*85000)"
         }
     }
 }
