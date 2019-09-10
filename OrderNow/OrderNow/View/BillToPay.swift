5adce28a653b4e12ce03792ac11c30267239b8a4
Thế Anh Huỳnh <theanh.petech@gmail.com>
2019-09-10 22:08:41 +0700

    10/9/2019 tableView

---
+++ OrderNow/OrderNow/View/BillToPay.swift
@@ -0,0 +1,16 @@
+//
+//  BillToPay.swift
+//  OrderNow
+//
+//  Created by admin on 9/7/19.
+//  Copyright © 2019 ForLearn. All rights reserved.
+//
+
+import UIKit
+
+struct Bill {
+    var foodImage: UIImage
+    var foodName: String
+    var foodNote: String
+    var foodPrice: String
+}
